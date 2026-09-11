aaa2477014bf7b75cae0a366c4bc7da7205e3ccc
Noonanski <an@hrsrail.co.uk>
2026-09-11 07:34:14 +0100

    Cache fixed off-peak TravelTime zone

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='hrs-travel-v1';
-const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
+const CACHE='hrs-travel-v2';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./zone/part-01.txt','./zone/part-02a.txt','./zone/part-02b.txt','./zone/part-02c.txt','./zone/part-03.txt','./zone/part-04a.txt','./zone/part-04b.txt','./zone/part-04c.txt'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match(e.request)))});
